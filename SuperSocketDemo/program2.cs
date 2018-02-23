9db0b4338c5b0ba4b1368455cfd3799b3a554899
TianJingsolo <1549153348@qq.com>
2018-02-23 21:23:56 +0800

    ...
    ...

---
+++ SuperSocketDemo/program2.cs
@@ -0,0 +1,103 @@
+﻿using SuperSocket.Common;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketEngine;
+using SuperSocket.SocketBase.Protocol;
+using System;
+using System.Linq;
+
+namespace SuperSocketDemo
+{
+    class Program2
+    {
+
+        /// <summary>
+        /// SuperSocket服务启动或停止
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start the server!");
+
+            Console.ReadKey();
+            Console.WriteLine();
+
+            var appServer = new AppServer();
+
+            //Setup the appServer
+            if (!appServer.Setup(2012)) //Setup with listening port
+            {
+                Console.WriteLine("Failed to setup!");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine();
+
+            //新连接事件  
+            appServer.NewSessionConnected += new SessionHandler<AppSession>(appServer_NewSessionConnected);
+            //收到消息事件  
+           // appServer.NewRequestReceived += new RequestHandler<AppSession, StringRequestInfo>(appServer_NewRequestReceived);
+            //连接断开事件  
+            appServer.NewRequestReceived += new RequestHandler<AppSession, StringRequestInfo>(appServer_NewRequestReceived);
+
+            //Try to start the appServer
+            if (!appServer.Start())
+            {
+                Console.WriteLine("Failed to start!");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("The server started successfully, press key 'q' to stop it!");
+            Console.WriteLine();
+
+            while (true)
+            {
+                var str = Console.ReadLine();
+                if (str.ToLower().Equals("q"))
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+
+            //Stop the appServer
+            appServer.Stop();
+
+            Console.WriteLine("The server was stopped!");
+            Console.ReadKey();
+        }
+
+        static void appServer_NewSessionConnected(AppSession session)
+        {
+            session.Send("Welcome to SuperSocket Telnet Server");
+        }
+
+        static void appServer_NewRequestReceived(AppSession session, StringRequestInfo requestInfo)
+        {
+            switch (requestInfo.Key.ToUpper())
+            {
+                case ("ECHO"):
+                    session.Send(requestInfo.Body);
+                    break;
+
+                case ("ADD"):
+                    session.Send(requestInfo.Parameters.Select(p => Convert.ToInt32(p)).Sum().ToString());
+                    break;
+
+                case ("MULT"):
+
+                    var result = 1;
+
+                    foreach (var factor in requestInfo.Parameters.Select(p => Convert.ToInt32(p)))
+                    {
+                        result *= factor;
+                    }
+
+                    session.Send(result.ToString());
+                    break;
+            }
+        }
+    }
+}
